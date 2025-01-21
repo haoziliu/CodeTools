4033b96c34fce6d7e66df50b8749848f47f1d2fb
Yunhao LIU <haoziliu@gmail.com>
2025-01-21 14:43:08 +0100

    daily

--- app/src/main/java/com/example/codetoolsnew/MatrixCode.kt
+++ app/src/main/java/com/example/codetoolsnew/MatrixCode.kt
@@ -1040,4 +1040,20 @@ object MatrixCode {
         fillRow(0)
         return result
     }
+
+    fun gridGame(grid: Array<IntArray>): Long {
+        val n = grid[0].size
+        var suffix0 = 0L
+        for (i in 1 until n) {
+            suffix0 += grid[0][i]
+        }
+        var result = suffix0
+        var prefix1 = 0L
+        for (i in 1 until n) {
+            suffix0 -= grid[0][i]
+            prefix1 += grid[1][i - 1]
+            result = minOf(result, maxOf(suffix0, prefix1))
+        }
+        return result
+    }
 }
